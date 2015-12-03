27f11a17de420f2ea4bab3e256a4f56b6cd39349
Montse Rodríguez Zamorano <montserzamorano@correo.ugr.es>
2015-12-03 10:41:45 +0100

    Update YodafyServidorIterativoUDP.java

--- practica2/ejercicio4/YodafyServidorIterativoUDP.java
+++ practica2/ejercicio4/YodafyServidorIterativoUDP.java
@@ -1,40 +1,40 @@
-package ejercicio1;
+package ejercicio4;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class YodafyServidorIterativoUDP {
 
-	public static void main(String[] args) {
+    public static void main(String[] args){
 	
-		// Puerto de escucha
-		int port=8989;
-		// array de bytes auxiliar para recibir o enviar datos.
-		byte []buffer=new byte[256];
-		// Número de bytes leídos
-		int bytesLeidos=0;
-    // Socket UDP
-    DatagramSocket socketServidor;
+	// Puerto de escucha
+	int port=8989;
+        // Socket UDP
+        DatagramSocket socketServidor = null;
 		
-		do{
-		  try{
-      // Abrimos el socket en modo pasivo, escuchando el en puerto indicado por "port"
-        socketServidor = new DatagramSocket(port); //inicializamos    
-      // no abrimos socket de servicio porque no es orientado a conexión
+	do{
+            try{
+                // Abrimos el socket en modo pasivo, escuchando el en puerto indicado por "port"
+                socketServidor = new DatagramSocket(port); //inicializamos    
 
-		  } catch (IOException e) {
-		  	System.err.println("Error al escuchar en el puerto "+port);
-		  }
+            } catch (IOException e) {
+		System.err.println("Error: no se pudo atender en el puerto  "+port);
+            }
 		  
 		  
-		  ProcesadorYodafy procesador = new ProcesadorYodafy(socketServidor);
-			procesador.procesa();
+            ProcesadorYodafy procesador = new ProcesadorYodafy(socketServidor);
+            try{
+                procesador.procesa();
+            } catch(IOException e){
+                System.err.println("Error al procesar la frase");
+            }
 			
-		} while (true);
+        } while (true);
 	
-	}
+    }
 
 }
